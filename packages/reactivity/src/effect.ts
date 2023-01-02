ca643a00c3a58ca57d44d9730b833e9f7d057aba
Xi-Yuer <2214380963@qq.com>
2023-01-02 11:28:01 +0800

    feat:一对多 Set 集合

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,3 +1,5 @@
+import { createDep, Dep } from './dep'
+
 export function effect<T = any>(fn: () => T) {
   const _effect = new ReactiveEffect(fn)
   // 第一次执行
@@ -14,7 +16,7 @@ export class ReactiveEffect<T = any> {
   }
 }
 
-type KeyToDepMap = Map<any, ReactiveEffect>
+type KeyToDepMap = Map<any, Dep>
 
 /**
  * 用于保存收集的依赖
@@ -30,12 +32,23 @@ const targetMap = new WeakMap<any, KeyToDepMap>()
  */
 export function track(target: object, key: unknown) {
   if (!activeEffect) return // 如果没有保存直接返回
-  let depsMap = targetMap.get(target)
+  let depsMap = targetMap.get(target) // {'name':{fn:()=>{} } }
   if (!depsMap) {
     // 如果不存在
     targetMap.set(target, (depsMap = new Map()))
   }
-  depsMap.set(key, activeEffect)
+  let dep = depsMap.get(key) // {fn:()=>{} }
+  if (!dep) {
+    depsMap.set(key, (dep = createDep()))
+  }
+  trackEffects(dep)
+}
+
+/**
+ * 利用 dep 依次跟踪指定 key 的 所有effect
+ */
+export function trackEffects(dep: Dep) {
+  dep.add(activeEffect!)
 }
 
 /**
@@ -46,13 +59,26 @@ export function track(target: object, key: unknown) {
  */
 export function trigger(target: object, key: unknown, newValue: unknown) {
   // 触发依赖其实就是执行 targetMap 中对应 effect然后执行对应的函数 fn
-  console.log('trigger:触发依赖')
-
   const depsMap = targetMap.get(target)
   if (!depsMap) return // 如果不存在直接返回
-  const effect = depsMap.get(key) as ReactiveEffect // 获取到对应的
-  if (!effect) return
+  const dep: Dep | undefined = depsMap.get(key) // 获取到对应的
+  if (!dep) return
 
   // 如果 effect 存在 则执行指定属性对应的函数（依赖触发）
-  effect.fn()
+  triggerEffects(dep)
+}
+
+/**
+ * 依次触发 dep 中保存的依赖
+ * @param dep
+ */
+export function triggerEffects(dep: Dep) {
+  const effects = Array.isArray(dep) ? dep : [...dep]
+  for (const effect of effects) {
+    triggerEffect(effect)
+  }
+}
+
+export function triggerEffect(effect:ReactiveEffect) {
+  effect.run()
 }
--- packages/vue/dist/vue.js
+++ packages/vue/dist/vue.js
@@ -1,6 +1,65 @@
 var Vue = (function (exports) {
     'use strict';
 
+    /******************************************************************************
+    Copyright (c) Microsoft Corporation.
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose with or without fee is hereby granted.
+
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+    REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+    INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+    LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+    OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+    PERFORMANCE OF THIS SOFTWARE.
+    ***************************************************************************** */
+
+    function __values(o) {
+        var s = typeof Symbol === "function" && Symbol.iterator, m = s && o[s], i = 0;
+        if (m) return m.call(o);
+        if (o && typeof o.length === "number") return {
+            next: function () {
+                if (o && i >= o.length) o = void 0;
+                return { value: o && o[i++], done: !o };
+            }
+        };
+        throw new TypeError(s ? "Object is not iterable." : "Symbol.iterator is not defined.");
+    }
+
+    function __read(o, n) {
+        var m = typeof Symbol === "function" && o[Symbol.iterator];
+        if (!m) return o;
+        var i = m.call(o), r, ar = [], e;
+        try {
+            while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+        }
+        catch (error) { e = { error: error }; }
+        finally {
+            try {
+                if (r && !r.done && (m = i["return"])) m.call(i);
+            }
+            finally { if (e) throw e.error; }
+        }
+        return ar;
+    }
+
+    function __spreadArray(to, from, pack) {
+        if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+            if (ar || !(i in from)) {
+                if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+                ar[i] = from[i];
+            }
+        }
+        return to.concat(ar || Array.prototype.slice.call(from));
+    }
+
+    var createDep = function (effects) {
+        var dep = new Set();
+        return dep;
+    };
+
     function effect(fn) {
         var _effect = new ReactiveEffect(fn);
         // 第一次执行
@@ -32,12 +91,22 @@ var Vue = (function (exports) {
     function track(target, key) {
         if (!activeEffect)
             return; // 如果没有保存直接返回
-        var depsMap = targetMap.get(target);
+        var depsMap = targetMap.get(target); // {'name':{fn:()=>{} } }
         if (!depsMap) {
             // 如果不存在
             targetMap.set(target, (depsMap = new Map()));
         }
-        depsMap.set(key, activeEffect);
+        var dep = depsMap.get(key); // {fn:()=>{} }
+        if (!dep) {
+            depsMap.set(key, (dep = createDep()));
+        }
+        trackEffects(dep);
+    }
+    /**
+     * 利用 dep 依次跟踪指定 key 的 所有effect
+     */
+    function trackEffects(dep) {
+        dep.add(activeEffect);
     }
     /**
      * 触发依赖
@@ -47,15 +116,38 @@ var Vue = (function (exports) {
      */
     function trigger(target, key, newValue) {
         // 触发依赖其实就是执行 targetMap 中对应 effect然后执行对应的函数 fn
-        console.log('trigger:触发依赖');
         var depsMap = targetMap.get(target);
         if (!depsMap)
             return; // 如果不存在直接返回
-        var effect = depsMap.get(key); // 获取到对应的
-        if (!effect)
+        var dep = depsMap.get(key); // 获取到对应的
+        if (!dep)
             return;
         // 如果 effect 存在 则执行指定属性对应的函数（依赖触发）
-        effect.fn();
+        triggerEffects(dep);
+    }
+    /**
+     * 依次触发 dep 中保存的依赖
+     * @param dep
+     */
+    function triggerEffects(dep) {
+        var e_1, _a;
+        var effects = Array.isArray(dep) ? dep : __spreadArray([], __read(dep), false);
+        try {
+            for (var effects_1 = __values(effects), effects_1_1 = effects_1.next(); !effects_1_1.done; effects_1_1 = effects_1.next()) {
+                var effect_1 = effects_1_1.value;
+                triggerEffect(effect_1);
+            }
+        }
+        catch (e_1_1) { e_1 = { error: e_1_1 }; }
+        finally {
+            try {
+                if (effects_1_1 && !effects_1_1.done && (_a = effects_1.return)) _a.call(effects_1);
+            }
+            finally { if (e_1) throw e_1.error; }
+        }
+    }
+    function triggerEffect(effect) {
+        effect.run();
     }
 
     var get = createGetter();
